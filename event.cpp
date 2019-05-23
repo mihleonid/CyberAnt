77617b1cec61d258271497d0c0cb7926481dc7be
MihLeonid <mihleonid@gmail.com>
2019-05-23 19:31:15 +0300

    Update event

--- event.cpp
+++ event.cpp
@@ -1,17 +1,6 @@
 #include "event.h"
 
-Event::Event(EventType t, Point p, FOWhat wh){
-	type=t;
-	what=wh;
-	pos=p;
-}
-Point Event::getPos(){
-	return pos;
-}
-EventType Event::getType(){
-	return type;
-}
-FOType Event::getWhat(){
-	return what;
+virtual bool Event::forGameController(){
+	return false;
 }
 
--- event.h
+++ event.h
@@ -1,19 +1,7 @@
 #pragma once
-class Event;
-#include "foenum.h"
-#include "point.h"
 
-enum EventType{EBuild, ERemove, EUpgrade};
-
-class Event{ // Нужно быдет изменить потом.
-	private:
-		Point pos;
-		EventType type;
-		FOWhat what;//ROxygen or BBase etc.
+class Event{
 	public:
-		Point getPos();
-		FOWhat getWhat();
-		EventType getType();
-		Event(EventType t, Point p, FOWhat wh);
+		virtual bool forGameController();
 };
 
