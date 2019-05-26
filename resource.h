4063b22ecc2f595e7577f3669e5412a03eb610a5
MihLeonid <mihleonid@gmail.com>
2019-05-26 19:02:50 +0300

    Remove includes

--- resource.h
+++ resource.h
@@ -1,8 +1,8 @@
 #pragma once
-class Resource;
+#include <string>
+
 //Типы ресурсов
 enum ResourceType{RIron, ROxygen, RCristall}; // При изменении нужно исправить resource.cpp, при желании --- создать возможность постройки в foenum, fieldmodelmodel, fieldview.
-#include <string>
 
 class Resource{
 	public:
--- resourceset.h
+++ resourceset.h
@@ -1,5 +1,4 @@
 #pragma once
-class ResourceSet;
 #include <map>
 #include "resource.h"
 
