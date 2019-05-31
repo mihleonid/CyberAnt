a97e4bfa2d67284544025a999d11d749f6098796
MihLeonid <mihleonid@gmail.com>
2019-05-31 20:25:57 +0300

    Configurator

---
+++ configurator.cpp
@@ -0,0 +1,10 @@
+#include "configurator.h"
+
+static void Configurator::configureResourceNode(ResourceType t, int& add, int& addP, int& taxMin, int& taxMax){
+	add=-1;
+	addP=10;
+	taxMin=0;
+	taxMax=20;
+	//TODO from files
+}
+
---
+++ configurator.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "foenum.h"
+
+class Configurator{
+	public:
+		static void configureResourceNode(ResourceType type, int& add, int& addPossible, int& taxMin, int& taxMax);
+}
+
