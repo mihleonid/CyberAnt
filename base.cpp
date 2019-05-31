6e4768d0bec3fdd3963d619ea1eef203d929fb2a
MihLeonid <mihleonid@gmail.com>
2019-05-31 19:40:45 +0300

    Base

---
+++ base.cpp
@@ -0,0 +1,23 @@
+#include "base.h"
+
+Base::Base(int xx, int yy, Field* f, int lvl):Building(xx, yy, f, lvl){
+	what=BBase;
+	type=(FOType)(FOBuilding|FOResourced|FOStorage|FOPutable);
+}
+void Base::update(){
+}
+SDL_Texture* Base::getTexture(Assets ass*, SDL_Renderer* ren){
+	return ass->tex(ass->getbase(), ren, ((Building*)this)->level);
+}
+bool Base::empty(){
+	return false;
+}
+void Base::put(Resourceset g){
+	have.add(g);
+}
+Resourceset Base::mine(Resourceset need){
+	Resourceset rest=have.sub(need);
+	need.sub(rest);
+	return need;
+}
+
---
+++ base.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "assets.h"
+#include "building.h"
+#include "field.h"
+#include "putable.h"
+#include "resourced.h"
+#include "resourceset.h"
+#include "storage.h"
+
+class Base:public Building, public Storage, public Resourced, public Putable{
+	public:
+		virtual void update();
+		virtual SDL_Texture* getTexture(Assets* ass, SDL_Renderer* ren);
+		Base(int xx, int yy, Field* f, int lvl);
+		bool empty();
+		void put(Resourceset g);
+		Resourceset mine(Resourceset need);
+};
+
