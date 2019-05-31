f2d6d0268fcf15cd3b45555ce3807c78e0a69fb0
MihLeonid <mihleonid@gmail.com>
2019-05-31 18:15:22 +0300

    Puttable FO

---
+++ putable.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "putable.h"
+
+virtual void Puttable::put(Resourceset give){
+	std::cerr<<"Calling to putable"<<std::endl;
+}
+
---
+++ putable.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "resourceset.h"
+
+class Putable{//можно давать
+	public:
+		virtual void put(Resourceset give);//дать ресурссет
+};
+
