b668369c7d8894c6c6ca2e658e9700ff737dda5c
MihLeonid <mihleonid@gmail.com>
2019-06-20 20:34:15 +0300

    Update random

--- random.cpp
+++ random.cpp
@@ -3,9 +3,27 @@
 
 void Random::init(){
 	srand(time(0));
+	current=0;
+	cyclesdone=CYCLES+1;
+}
+void Random::recycle(){
+	if(cyclesdone>CYCLES){
+		current=0;
+		cyclesdone=0;
+		for(int i=0;i<SIZE;++i){
+			rands[i]=rand();
+		}
+	}else{
+		++current;
+		if(current>=SIZE){
+			current=0;
+			++cyclesdone;
+		}
+	}
 }
 int Random::rnd(){
-	return rand();
+	recycle();
+	return rands[current];
 }
 int Random::rnd(int max){
 	return rnd(1, max);
--- random.h
+++ random.h
@@ -1,10 +1,20 @@
 #pragma once
 
 class Random{
+	private:
+		static const int SIZE=2000; // Big value
+		static const int CYCLES=20; // Smaller - better random, bigger - faster
+		static int current;
+		static int cyclesdone;
+		static int rands[SIZE];
+		static void recycle();
 	public:
 		static void init();
 		static int rnd();
 		static int rnd(int max); // From 1 to max
 		static int rnd(int min, int max); // From min to max
 };
+int Random::current;
+int Random::cyclesdone;
+int Random::rands[SIZE];
 
