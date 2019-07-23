c949cce33730e44e41081b995bff0d25cad71206
MihLeonid <mihleonid@gmail.com>
2019-07-23 15:58:40 +0300

    Automatic tests

--- tests/testd.sh
+++ tests/testd.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-g++ tester.cpp
-mv a.out tester
-for file in tests/*.cpp;
-	do
-		./tester $file
-		cat tmp1.txt>>atop.txt
-		cat tmp2.txt>>amiddle.txt
-		rm tmp1.txt tmp2.txt
-	done
-rm tester
-cat top.txt>main.cpp
-cat atop.txt>>main.cpp
-cat middle.txt>>main.cpp
-cat amiddle.txt>>main.cpp
-cat bottom.txt>>main.cpp
-rm "atop.txt"
-rm "amiddle.txt"
+bash pretest.sh
 
 g++ -g -ggdb "main.cpp" -c `sdl2-config --cflags --libs` -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_ttf -lpthread
 g++ -g -ggdb -o "main" "main.o" `sdl2-config --cflags --libs` -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_ttf -lpthread
