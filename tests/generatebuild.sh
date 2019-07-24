2e15b439ceaf0e253ea426c2e99aedf3cd158276
MihLeonid <mihleonid@gmail.com>
2019-07-24 21:36:20 +0300

    Update tests generation

--- tests/generatebuild.sh
+++ tests/generatebuild.sh
@@ -2,6 +2,6 @@
 
 echo "Generating build file..."
 cat buildtop.txt > build.cpp
-find ../src |grep "\.cpp"|grep -v "Make"|grep -v "main.cpp"|sed "i\#include["|xargs -n 2 echo|sed "s/ //"|sed "a]"|xargs -n 2 echo|sed "s/ //"|sed "s/\[/ \"/"|sed "s/\]/\"/"|sort >>build.cpp
+find ../src |grep "\.cpp"|grep -v "\.swp"|grep -v "Make"|grep -v "main.cpp"|sed "i\#include["|xargs -n 2 echo|sed "s/ //"|sed "a]"|xargs -n 2 echo|sed "s/ //"|sed "s/\[/ \"/"|sed "s/\]/\"/"|sort >>build.cpp
 echo "Generated"
 
