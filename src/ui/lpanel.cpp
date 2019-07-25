0844e45e6d776539907564fa629b4b76b633255f
MihLeonid <mihleonid@gmail.com>
2019-07-25 23:43:25 +0300

    LPanel optimization

--- src/ui/lpanel.cpp
+++ src/ui/lpanel.cpp
@@ -1,6 +1,7 @@
 #include "lpanel.h"
 #include "lscene.h"
 #include "levent.h"
+#include "lmouseevent.h"
 
 LPanel::LPanel(Rect p):LRectedComponent(p){
 	scn=new LScene();
@@ -25,6 +26,11 @@ std::vector<LEventType> LPanel::acceptedTypes(){
 	return LEvent::getAllTypes();
 }
 EventQueue LPanel::applyEvent(LEvent* e){
+	if(e->getType()==Mouse){
+		if(!rect.contain(((LMouseEvent*)e)->getPos())){
+			return EventQueue();
+		}
+	}
 	return scn->applyEvent(e);
 }
 
