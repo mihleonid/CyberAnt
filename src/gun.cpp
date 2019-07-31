88305a393f4a236ab5495b04ce787368908e4716
MihLeonid <mihleonid@gmail.com>
2019-07-31 21:22:34 +0300

    Update gun

--- src/gun.cpp
+++ src/gun.cpp
@@ -1,11 +1,14 @@
+#include <iostream>
 #include "gun.h"
 #include "field.h"
 #include "random.h"
 
 Gun::Gun(Point p, Field* f, int lvl):Building(p, f, lvl){
 	what=BBase;
-	type=FOBuilding;
+	type=FOBuilding|FOPutable;
 	strength=1;
+	cost=1;
+	iron=0;
 }
 void Gun::update(){
 	for(Point p:getField()->getnb(getPos())){
@@ -14,10 +17,17 @@ void Gun::update(){
 			continue;
 		}
 		if(f->getType()&FOEnemy){
-			if(Random::rnd(23)==1){
-				if(f->damage(strength)){
-					++strength;
+			if(iron>10){
+				iron-=10;
+				if(Random::rnd(10)==1){
+					if(f->damage(strength)){
+						++strength;
+					}
 				}
+			}else{
+#ifdef DEBUG
+				std::cout<<"Gun is inactive"<<std::endl;
+#endif
 			}
 		}
 	}
@@ -25,11 +35,15 @@ void Gun::update(){
 bool Gun::upgrade(){
 	if(Building::upgrade()){
 		++strength;
+		++cost;
 		return true;
 	}
 	return false;
 }
 std::pair<LImage*, LColor> Gun::getImage(Assets* ass) const{
 	return std::pair<LImage*, LColor>(ass->get("gun"), levelColor());
+}
+void Gun::put(ResourceSet g){
+	iron+=g.get(Iron);
 }
 
--- src/gun.h
+++ src/gun.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "building.h"
+#include "putable.h"
 
-class Gun:public Building{
+class Gun:public Building, public Putable{ // Зарежается железом.
 	private:
 		int strength;
+		int iron;
+		int cost;
 	public:
 		Gun(Point, Field*, int lvl);
 		virtual void update();
 		virtual bool upgrade();
 		virtual std::pair<LImage*, LColor> getImage(Assets* ass) const;
+		virtual void put(ResourceSet give);
 };
 
